a5f0ee0987fbba4a7a41d0145c4e8c52c64be365
Ercan <ercann.6191@gmail.com>
2024-05-29 20:59:00 +0300

    new commit

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,3 +1,17 @@
+const accountsModel = require("./accounts-model");
+const yup = require("yup");
+
+const accountsShema = yup.object().shape({
+  name:yup.string()
+  .required("name alanı zorunludur")
+  .min(3,"minimum 3 karakter girmelisiniz")
+  .max(100,"maksimum 100 karakter olamalı"),
+  budget:yup.number("Budget alanı number olmalıdır")
+  .required("Budget alanı zorunludur.")
+  .min(0,"Bütçe negtif değer olamaz")
+  .max(1000000,"bütçe bir milyondan büyük olamaz")
+});
+
 exports.checkAccountPayload = (req, res, next) => {
   // KODLAR BURAYA
   // Not: Validasyon için Yup(şu an yüklü değil!) kullanabilirsiniz veya kendiniz manuel yazabilirsiniz.
